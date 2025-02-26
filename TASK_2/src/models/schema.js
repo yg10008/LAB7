7e1835a42369b4f64060658057a0003821c3d311
yg10008 <trilokay3@gmail.com>
2025-02-26 23:11:05 +0530

    Initial commit - Task 1 completed

---
+++ TASK_2/src/models/schema.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 3,
+            maxLength: 100,
+        },
+        description: {
+            type: String,
+            trim: true,
+            maxLength: 500,
+        },
+        status: {
+            type: String,
+            enum: ["Pending", "Completed"],
+            default: "Pending",
+        },
+        dueDate: {
+            type: Date,
+        },
+    },
+    {
+        timestamps: true, 
+    }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = {
+    Task,
+};
